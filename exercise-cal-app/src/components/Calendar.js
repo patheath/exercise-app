e5d41180d9ea4f276158d1f593a4dfc6c66104f2
patheath <patheath@gmail.com>
2019-11-15 08:11:55 -0500

    built the dynamic days for calendar

--- exercise-cal-app/src/components/Calendar.js
+++ exercise-cal-app/src/components/Calendar.js
@@ -10,8 +10,40 @@ class Calendar extends Component {
     this.state = { 
       month: today.getMonth(), 
       year: today.getFullYear(),
-      months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+      months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      days: days
     };
+  }
+
+  buildDays = (startDay, daysInMonths) => {
+    let tableRows = [];
+    let day = 1;
+    for (var w = 0; w < 5; w++) {  // weeks
+      let days = [];
+
+      for (var d = 0; d < 7; d++) {  // days
+        if (w === 0) {
+          // start of month
+          if (d >= startDay) {
+            days.push(<td>{day}</td>);
+            day++;
+          } else {
+            days.push(<td class="nil"></td>);
+          }
+        } else { 
+          // check end of month
+          if (day <= daysInMonths) {
+            days.push(<td>{day}</td>);
+            day++;
+          } else {
+            days.push(<td class="nil"></td>);
+          }
+        }
+      }
+      tableRows.push(<tr>{days}</tr>);
+    }
+  return tableRows;
+
   }
 
   switchMonth = (forward) => {
@@ -30,16 +62,16 @@ class Calendar extends Component {
 
     // How long is the month? Move one month in future then subtract 1 day and see 
     // how many days in the month
-    const nextMonthDateString = (month == 11) ? this.state.months[0] + " " + (year + 1) : this.state.months[month + 1] + " " + year;
+    const nextMonthDateString = (month === 11) ? this.state.months[0] + " " + (year + 1) : this.state.months[month + 1] + " " + year;
     const selectedMonth = new Date(nextMonthDateString);
     selectedMonth.setDate(0);  // set to last day in selected month
     const daysInMonths = selectedMonth.getDate();
     selectedMonth.setDate(1);  // set to first day in the selected month
     const startingDay = selectedMonth.getDay();
+    const dayElement = this.buildDays(startingDay, daysInMonths);
 
     // have my starting day and my length of month let's draw!
-    
-    this.setState({ month: month, year: year});
+    this.setState({ month: month, year: year, days: dayElement});
   }
 
   next = () => {
@@ -49,6 +81,7 @@ class Calendar extends Component {
   previous = () => {
     this.switchMonth(false);
   }
+
 /*
   exerciseCalendar = () => { 
     var wrap, 
@@ -173,13 +206,7 @@ class Calendar extends Component {
         </table> 
         <div id="cal-frame"> 
           <table class="curr"> 
-              <tbody> 
-                  <tr><td class="nil"></td><td class="nil"></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr> 
-                  <tr><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td class="today">11</td><td>12</td></tr> 
-                  <tr><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td></tr> 
-                  <tr><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td></tr> 
-                  <tr><td>27</td><td>28</td><td>29</td><td>30</td><td class="nil"></td><td class="nil"></td><td class="nil"></td></tr> 
-              </tbody> 
+              <tbody>{this.state.days}</tbody> 
           </table>
                   </div> 
       </div>
